36c392d363d3486de2f1b22b7cf8a466bbf3a6a1
Joanna Huang <joanna.huitzu.huang@gmail.com>
2012-02-16 18:03:35 +1100

    fixed settings

--- settings_microtardis.py
+++ settings_microtardis.py
@@ -1,6 +1,9 @@
 from os import path
 from tardis.settings_changeme import *
 
+# Debug mode
+DEBUG = False
+
 # Database settings
 DATABASES = {
     'default': {
@@ -55,6 +58,32 @@ TEMPLATE_LOADERS = (
 MT_STATIC_URL_ROOT = '/static'
 MT_STATIC_DOC_ROOT = path.join(path.dirname(__file__),
                                'apps/microtardis/static').replace('\\', '/')
-                               
+
+# smatplotlib module configuration                               
 MATPLOTLIB_HOME = path.abspath(path.join(path.dirname(__file__), 
                                          '../')).replace('\\', '/')
+# LDAP configuration
+LDAP_USE_TLS = False
+LDAP_URL = "ldap://localhost:38911/"
+LDAP_USER_LOGIN_ATTR = "uid"
+LDAP_USER_ATTR_MAP = {"givenName": "display", "mail": "email"}
+LDAP_GROUP_ID_ATTR = "cn"
+LDAP_GROUP_ATTR_MAP = {"description": "display"}
+#LDAP_ADMIN_USER = ''
+#LDAP_ADMIN_PASSWORD = ''
+LDAP_BASE = 'dc=example, dc=com'
+LDAP_USER_BASE = 'ou=People, ' + LDAP_BASE
+LDAP_GROUP_BASE = 'ou=Group, ' + LDAP_BASE
+
+# Post Save Filters
+POST_SAVE_FILTERS = [
+    ("tardis.apps.microtardis.filters.exiftags.make_filter", ["MICROSCOPY_EXIF","http://rmmf.isis.rmit.edu.au/schemas"]),
+    ("tardis.apps.microtardis.filters.spctags.make_filter", ["EDAXGenesis_SPC","http://rmmf.isis.rmit.edu.au/schemas"]),
+    ]
+
+# Priviate datafiles
+PRIVATE_DATAFILES = False
+
+# Staging Protocol
+STAGING_PROTOCOL = 'localdb'
+GET_FULL_STAGING_PATH_TEST = path.join(STAGING_PATH, "test_user")
